d2fa8503b8d91169c45d4063395f979a36f94053
BL-Rohan <rohan.kadam@bridgelabz.com>
2020-07-02 18:34:17 +0530

    ADDED Launcher Activity and Started Topic Activity.

--- app/src/main/java/com/spatalabz/programme/MainActivity.java
+++ app/src/main/java/com/spatalabz/programme/MainActivity.java
@@ -2,7 +2,9 @@ package com.spatalabz.programme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void topicContent(View view) {
+        Intent topicIntent=new Intent(MainActivity.this,Topic.class);
+        startActivity(topicIntent);
     }
 }
---
+++ app/src/main/java/com/spatalabz/programme/Topic.java
@@ -0,0 +1,14 @@
+package com.spatalabz.programme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Topic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic);
+    }
+}
