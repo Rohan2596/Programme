1d2434e9eff983a99712f316433fe81649016f8c
BL-Rohan <rohan.kadam@bridgelabz.com>
2020-07-04 20:22:03 +0530

    ADDED:Arrow Navigation to Content Activity

--- app/src/main/java/com/spatalabz/programme/Content.java
+++ app/src/main/java/com/spatalabz/programme/Content.java
@@ -1,14 +1,21 @@
 package com.spatalabz.programme;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class Content extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
