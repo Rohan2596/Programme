5c2331a26ba3dcf81765bd9472ff69aa1e0d22eb
BL-Rohan <rohan.kadam@bridgelabz.com>
2020-07-07 20:33:18 +0530

    ADDED:ListView to content activity and needs to work on logic.

--- app/src/main/java/com/spatalabz/programme/Content.java
+++ app/src/main/java/com/spatalabz/programme/Content.java
@@ -6,12 +6,18 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Content extends AppCompatActivity {
 
     Toolbar toolbar;
     Intent intent;
     String techTitle;
+    ListView simpleList;
+    //"SHELLSCRIPT PROGRAMMING","JAVA PROGRAMMING"
+    String javaList[]={"1. Java Introduction", "China", "australia", "Portugle"};
+    String shelllists[]={"1. Shell Introduction", "australia", "Portugle", "America", "NewZealand"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,22 @@ public class Content extends AppCompatActivity {
         techTitle=intent.getStringExtra("TechStack");
         getSupportActionBar().setTitle(techTitle);
        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        if(techTitle.equals("SHELLSCRIPT PROGRAMMING")){
+            simpleList = (ListView)findViewById(R.id.contentList);
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.content_list, R.id.textView, shelllists);
+            simpleList.setAdapter(arrayAdapter);
+
+        }
+        if(techTitle.equals("JAVA PROGRAMMING")){
+            simpleList = (ListView)findViewById(R.id.contentList);
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.content_list, R.id.textView, javaList);
+            simpleList.setAdapter(arrayAdapter);
+
+        }
+
     }
+
+
 }
